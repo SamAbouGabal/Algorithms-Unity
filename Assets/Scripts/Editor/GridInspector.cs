93d00c04245ec80bb95a8611f06721f49682d975
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2024-05-20 15:49:00 +0200

    Update GridInspector.cs

--- Assets/Scripts/Editor/GridInspector.cs
+++ Assets/Scripts/Editor/GridInspector.cs
@@ -11,17 +11,16 @@ namespace Editor
         public override void OnInspectorGUI()
         {
             base.OnInspectorGUI();
-           
-                cellPrefab = EditorGUILayout.ObjectField("Cell prefab", cellPrefab, typeof(GridCell));
-                EditorGUI.BeginDisabledGroup(cellPrefab != null);
-                if (GUILayout.Button("Generate Grid"))
-                {
-                    Grid grid = target as Grid;
-                    EditorUtility.SetDirty(grid);
-                }
-                EditorGUI.EndDisabledGroup();
-                GUI.enabled = true;
-                GUILayout.Button("Hello");
+            cellPrefab = EditorGUILayout.ObjectField("Cell prefab", cellPrefab, typeof(GridCell));
+            EditorGUI.BeginDisabledGroup(cellPrefab != null);
+            if (GUILayout.Button("Generate Grid"))
+            {
+                Grid grid = target as Grid;
+                EditorUtility.SetDirty(grid);
+            }
+            EditorGUI.EndDisabledGroup();
+            GUI.enabled = true;
+            GUILayout.Button("Hello");
         }
     }
 }
