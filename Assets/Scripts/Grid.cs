068090a893e7144c5943aff11359ca770d507885
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2024-05-20 14:45:59 +0200

    Update Grid.cs

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -4,6 +4,15 @@ using UnityEngine;
 
 public class Grid : MonoBehaviour
 {
+
+    public bool[] walkableGrid = new bool[100];
+    public int width;
+    
+    public bool isWalkable(int x, int y)
+    {
+        return walkableGrid[y * width + x];
+    }
+    
     // Start is called before the first frame update
     void Start()
     {
