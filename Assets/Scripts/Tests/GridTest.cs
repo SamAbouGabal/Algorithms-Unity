778e3648673529a7090dee2528c5938f53c8f2dd
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2024-05-21 13:39:17 +0200

    Update GridTests

--- Assets/Scripts/Tests/GridTest.cs
+++ Assets/Scripts/Tests/GridTest.cs
@@ -1,12 +1,26 @@
 using NUnit.Framework;
 using UnityEditor;
 using UnityEngine;
+using Grid = Grids.Grid;
 
 namespace Tests
 {
     public class GridTest
     {
         private Grid grid;
+
+        [SetUp]
+        public void SetUp()
+        {
+            
+        }
+
+        [TearDown]
+
+        public void TearDown()
+        {
+            
+        }
 
         [Test]
         public void GridTestSimplePasses()
