c2d91caf972de498e1a4a6e9b8a61463d7ec75c1
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2024-05-21 11:31:49 +0200

    Update PlayerController.cs

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -10,9 +11,30 @@ public class PlayerController : MonoBehaviour
         
     }
 
-    // Update is called once per frame
     void Update()
     {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Grid grid = FindObjectOfType<Grid>();
+            GridCell start = null;
+            GridCell end = null;
+            var path = FindPath(grid, start, end);
+            // start coroutine
+            //     traverse the path
+        }
+    }
+
+    // Update is called once per frame
+    static IEnumerable<GridCell> FindPath(Grid grid, GridCell start, GridCell end)
+    {
+        // track visited cells
+        // track cells that need to be visited
+        // track the goal
+        // decide what cells to visit next
         
+        // if current cell == goal
+        // reconstruct path
+        // return path
+        throw new NotImplementedException();
     }
 }
