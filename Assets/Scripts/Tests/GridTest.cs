784715e43cad009055ab7791aa963558a19af704
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2024-05-21 13:46:11 +0200

    Update GridTest.cs

--- Assets/Scripts/Tests/GridTest.cs
+++ Assets/Scripts/Tests/GridTest.cs
@@ -1,3 +1,4 @@
+using Grids;
 using NUnit.Framework;
 using UnityEditor;
 using UnityEngine;
@@ -12,14 +13,32 @@ namespace Tests
         [SetUp]
         public void SetUp()
         {
-            
+            var gridGameObject = new GameObject("Grid");
+            grid = gridGameObject.AddComponent<Grid>();
+            grid.width = 3;
+            int height = 3;
+            grid.walkableGrid = new GridCell[9];
+            int i = 0;
+            for (int y = 0; y < height; y++)
+            {
+                for (int x = 0; x < grid.width; x++)
+                {
+                    var cellGameObject = new GameObject("GridCell");
+                    cellGameObject.transform.SetParent(grid.transform);
+                    cellGameObject.transform.position = new Vector3(x, y, 0);
+                    cellGameObject.AddComponent<SpriteRenderer>();
+                    var cell = cellGameObject.AddComponent<GridCell>();
+                    grid.walkableGrid[i++] = cell;
+                }
+            }
         }
 
         [TearDown]
 
         public void TearDown()
         {
-            
+            GameObject.DestroyImmediate(grid);
+            grid = null;
         }
 
         [Test]
