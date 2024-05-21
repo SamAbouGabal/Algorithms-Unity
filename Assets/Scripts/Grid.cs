f0aa1fb079837f1ce0aefc51019221844c1c1a79
Sam Abou-Gabal <sam.abougabal@student.forsbergsskola.se>
2024-05-21 11:31:42 +0200

    Update to Grid Script

--- Assets/Scripts/Grid.cs
+++ Assets/Scripts/Grid.cs
@@ -1,26 +1,23 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
-
 
 public class Grid : MonoBehaviour
 {
-
     public GridCell[] walkableGrid = new GridCell[100];
-    public int width;
+    public int width = 10;
     
-    public bool isWalkable(int x, int y)
+    public bool IsWalkable(int x, int y)
     {
-        return walkableGrid[y * width + x].walkable;
-    }
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+        return walkableGrid[y*width+x].walkable;
     }
 
-    // Update is called once per frame
-    void Update()
+    public GridCell GetCellForPosition(Vector3 position)
     {
-        
+        // remember: floats are inaccurate, so you can't do position.x == 4
+        // how can you calculate the right x and y index for the Vector3 position?
+        // then, return the cell at that index of the array
+        throw new NotImplementedException();
     }
 }
